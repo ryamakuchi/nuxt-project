c7770d091608ecfadd3bac7c347fb1de0e02bdd8
ryamakuchi <r.yamakuchi@gmail.com>
2018-10-06 16:43:48 +0900

    はじめてのVuex

---
+++ plugins/axios.js
@@ -0,0 +1,7 @@
+export default function ({ $axios }) {
+  $axios.onRequest( (config) =>{
+   if (config.url.indexOf('api.github.com') +1 ) {
+     config.headers.Authorization = `token 711b20ec592e7cc959938ff5837f739b1dbf1c04`
+   }
+  })
+}
